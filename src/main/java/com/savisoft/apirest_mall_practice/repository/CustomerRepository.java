c15b0c11d3b87f0f23ca32a447f1f466ea53a615
Santiago Avila <95515422+Sanavi01@users.noreply.github.com>
2025-01-28 18:33:29 -0500

    Metodos JPA repositories para diferentes casos

--- src/main/java/com/savisoft/apirest_mall_practice/repository/CustomerRepository.java
+++ src/main/java/com/savisoft/apirest_mall_practice/repository/CustomerRepository.java
@@ -4,8 +4,15 @@ import com.savisoft.apirest_mall_practice.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository  extends JpaRepository<Customer,Long> {
 
+    Optional<Customer> findByName(String name);
+    List<Customer> findByNameContaining(String name); //Trae todos los objetos cuyo nombre tengan los caracteres llevados en el String name
+    List<Customer> findByLastNameNotNull(); //Trae todos los registros que no tengan null en lastName
+    //Todas estos metodos custom se pueden sacar de la documentacion de SpringBoot "Query Methods", expecificamente query creation
 
 }
